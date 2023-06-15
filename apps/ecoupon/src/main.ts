2f82c1fd7074e636f765fbbf2dd47ae93fb77640
ductruong253 <ddtruong94@gmail.com>
2023-06-15 15:26:41 +0700

    add cors for ecoupon auth api

--- apps/ecoupon/src/main.ts
+++ apps/ecoupon/src/main.ts
@@ -3,8 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: 'http://localhost:3002',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders:
+      'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials',
+  });
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
     res.removeHeader('date');
